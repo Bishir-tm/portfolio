db2609d779287033cafb60be524cc9b5ee12fe60
Bishir T M <bishirtijjani247@gmail.com>
2023-01-11 06:35:27 +0100

    initial commit before github pages upload

---
+++ js/setAnime.js
@@ -0,0 +1,17 @@
+anime({
+    targets: ".slideFromLeft",
+    duration: 3000,
+    keyframes:[ 
+        {width: '0'},
+        {fontSize: '0'},
+        {borderRadius:'50px'},
+    ],
+    // translateX:[-40, 7],
+    direction:'alternate',
+    // loop: true,
+    easing:   'easeOutBounce',
+    borderRadius: '50px'
+
+
+
+});
---
+++ js/setSlick.js
@@ -0,0 +1,42 @@
+
+// slick home carousel
+$(document).ready(function(){
+    $('.portfolio-Projects-Carousel').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        dots:true,
+        infinite: true,
+        speed: 50,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        variableWidth:true,
+        autoplay: true,
+        arrows: true,
+        autoplayspeed: 50,
+        cssEase: 'linear',
+        focusOnselect: true,
+        focusOnSelect: true,
+        edgeFriction: 0,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ]
+        
+}); 
+})
